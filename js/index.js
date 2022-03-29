f851a34d35634b3363688033b6ff205a3aa03b72
TheSas99 <TheSas99@users.noreply.github.com>
2022-03-29 15:46:10 +0200

    Added footer and Header

---
+++ js/index.js
@@ -0,0 +1,15 @@
+function addFooter() {
+    document.getElementById("Footer").innerHTML += 
+      "<h3>Contact</h3><br>"
+      "<p><b>Adres</b></p>"
+      "<p>Oasen N.V.</p>"
+      "<p>Nieuwe Gouwe O.Z. 3</p>"
+      "<p>2801 SB Gouda</p></p>"
+      "<p><b>Telefoonnummer</b></p>"
+      "<p>0182 - 59 35 30";
+}
+
+function addHeader() {
+    document.getElementById("Footer").innerHTML += 
+      "<h1>Oasen</h1><br>";
+}
