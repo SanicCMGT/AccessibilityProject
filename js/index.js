8cdb8be75e5f23fad9b66f3c6139010efa9688d7
TheSas99 <TheSas99@users.noreply.github.com>
2022-03-29 15:55:53 +0200

    Added New Updates to footer

--- js/index.js
+++ js/index.js
@@ -1,15 +1,15 @@
 function addFooter() {
-    document.getElementById("Footer").innerHTML += 
+    document.getElementById("footer").innerHTML += 
       "<h3>Contact</h3><br>"
       "<p><b>Adres</b></p>"
       "<p>Oasen N.V.</p>"
       "<p>Nieuwe Gouwe O.Z. 3</p>"
       "<p>2801 SB Gouda</p></p>"
       "<p><b>Telefoonnummer</b></p>"
-      "<p>0182 - 59 35 30";
+      "<p>0182 - 59 35 30</p>";
 }
 
 function addHeader() {
-    document.getElementById("Footer").innerHTML += 
+    document.getElementById("header").innerHTML += 
       "<h1>Oasen</h1><br>";
 }
